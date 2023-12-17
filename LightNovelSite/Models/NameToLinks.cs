61e3ee34b0fc72bf17f801bc7825d1631f8f9edb
Freelife05 <milevstilqn13@gmail.com>
2023-12-17 23:43:28 +0200

    Added Name to links function

---
+++ LightNovelSite/Models/NameToLinks.cs
@@ -0,0 +1,10 @@
+﻿using MessagePack;
+
+namespace LightNovelSite.Models
+{
+    public class NameToLinks
+    {
+        public string Name { get; set; }
+        public string Links { get; set; }
+    }
+}
